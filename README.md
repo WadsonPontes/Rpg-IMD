e5b32480bc66f9f4a11c7b9b06e515584efa297b
fafato1 <fafato.889@gmail.com>
2017-10-13 22:32:50 -0300

    teste
    Rpg para estudar

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RPG
-"# Rpg-IMD" 
+"# Este é um RPG que eu estou criando em cooperação com Pedro Garcia e Matheus Sueht, então espero que quem quiser estudar ou ajudar é só entrar em contato"
---
+++ RpgImd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RpgImd {
+
+    public static Scanner leitor = new Scanner(System.in);
+
+    public static void main(String[] args) {
+      Scanner leitor = new Scanner(System.in);
+      int valor = leitor.nextInt();
+
+
+      if (valor == 2) {
+      System.out.print("");
+      }
+
+
+    }
+
+}
