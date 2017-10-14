9d403eb4ed4ea962898cce5e2249b15059f8b67d
fafato1 <fafato.889@gmail.com>
2017-10-14 19:39:43 -0300

    arrumando os dados

--- RpgImd.java
+++ RpgImd.java
@@ -4,32 +4,62 @@ import java.util.Random;
 public class RpgImd {
 
     public static Scanner leitor = new Scanner(System.in);
-    public static int dificuldade;
+    public static int dificuldade = 5;
     public static Random gerador = new Random();
 
+/* como usar o leitor
+  int linha = leitor.nextInt();
+  System.out.println(linha);
+*/
 
     public static void main(String[] args) {
-      int linha = leitor.nextInt();
-      System.out.println(linha);
+      menu();
     }
 
-    public static void Menu(){
-      System.out.println("Menu");
-      System.out.println("Digite 1 para iniciar");
+
+
+    public static void menu(){
+      System.out.println("\tMenu\n");
+      System.out.println("Digite \"iniciar\" para iniciar");
     }
 
-    public static int d20(){
+
+
+    public static void d20(){
       int dado20 = gerador.nextInt(20)+1;
-      return dado20;
+      if (dado20 > dificuldade){
+      System.out.println("d20 SUCESSO =" + dado20);
+      }
+
+      else{
+        System.out.println("FALHA");
+      }
+
     }
 
-    public static int d10(){
+
+
+    public static void d10(){
       int dado10 = gerador.nextInt(10)+1;
-      return dado10;
+      if (dado10 > dificuldade){
+      System.out.println("d10 SUCESSO =" + dado10);
+      }
+
+      else{
+        System.out.println("FALHA");
+      }
     }
 
-    public static int d6(){
+
+
+    public static void d6(){
       int dado6 = gerador.nextInt(6)+1;
-      return dado6;
+      if (dado6 > dificuldade){
+      System.out.println("d6 SUCESSO =" + dado6);
+      }
+
+      else{
+        System.out.println("FALHA");
+      }
     }
 }
