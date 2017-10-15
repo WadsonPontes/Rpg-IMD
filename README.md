479d64c32369a8bfde98c917e6ecd063c8230015
fafato1 <fafato.889@gmail.com>
2017-10-14 22:30:52 -0300

    mais um update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # RPG
-"# Este é um RPG que eu estou criando em cooperação com Pedro Garcia e Matheus Sueht, então espero que quem quiser estudar ou ajudar é só entrar em contato"
+"# Este é um RPG que eu estou criando em cooperação com Pedro Garcia e Matheus Sueht, então espero que gostem do projeto e se quiser estudar ou ajudar é só entrar em contato"
--- RpgImd.java
+++ RpgImd.java
@@ -11,26 +11,27 @@ public class RpgImd {
     public static String classe;
     public static int level;
     public static int exp;
+    public static String Tela;
 
 /* como usar o leitor
-  int linha = leitor.nextInt();
-  System.out.println(linha);
+  tipo nome = leitor.nextTipo();
+  System.out.println(nome);
 */
 
     public static void main(String[] args) {
-      menu();
+      
     }
 
 
 
-    public static void menu(){
+    public static void menu(){ //Menu do RPG
       System.out.println("\tMenu\n");
       System.out.println("Digite \"iniciar\" para iniciar");
     }
 
 
 
-    public static void d20(){
+    public static void d20(){ //Dado de 20 lados
       int dado20 = gerador.nextInt(20)+1;
       if (dado20 > dificuldade){
       System.out.println("d20 SUCESSO =" + dado20);
@@ -44,7 +45,7 @@ public class RpgImd {
 
 
 
-    public static void d10(){
+    public static void d10(){ //Dado de 10 lados
       int dado10 = gerador.nextInt(10)+1;
       if (dado10 > dificuldade){
       System.out.println("d10 SUCESSO =" + dado10);
@@ -57,7 +58,7 @@ public class RpgImd {
 
 
 
-    public static void d6(){
+    public static void d6(){ //Dado de 6 lados
       int dado6 = gerador.nextInt(6)+1;
       if (dado6 > dificuldade){
       System.out.println("d6 SUCESSO =" + dado6);
@@ -67,4 +68,19 @@ public class RpgImd {
         System.out.println("FALHA");
       }
     }
+
+
+
+    public static void morte(){ //tela de morte
+      System.out.println("Voce morreu, seu boboca");
+    }
+
+
+
+    public static void telainicial(){
+      System.out.println("");
+    }
+
+
+
 }
