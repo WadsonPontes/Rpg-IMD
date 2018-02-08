586a52380a9ef5716d0fd68e2e7ec248a9c4deff
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2018-02-08 18:01:53 -0300

    Update fuction obterExperienciaMaxima()
    Você vai precisar alterar as outras funções para o caso de o level esteja no máximo. Com level no máximo não se pode mais ganhar experiência e a experiência deve zerar para ficar 0/0.
    A função obterExperienciaMaxima() foi balanceada para o caso de os monstros darem entre 10 e 1.000 de experiência.

--- com/main/SerVivoComLevel.java
+++ com/main/SerVivoComLevel.java
@@ -4,6 +4,14 @@ public class SerVivoComLevel extends SerVivo{
   public double level;
 
   public double obterExperienciaMaxima(){
-    return Math.round(Math.floor(Math.pow(level, 2) * 25));
+    
+    if (level == 100) return 0;
+    
+    switch(classeNumero){
+      case 1: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 267) / ((level + 4) * 6) ); break;
+      case 2: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 270) / ((level + 4) * 6.1) ); break;
+      case 3: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 270)/ ((level + 4) * 6.2) ); break;
+      default: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 600)/ ((level + 4) * 9) ); break;
+    }
   }
 }
