fd1b041d2b148e1ac4b9c11d3087afe2060842e9
fafato1 <fafato.889@gmail.com>
2018-08-22 00:34:58 -0300

    Organizando

--- com/main/SerVivoComLevel.java
+++ com/main/SerVivoComLevel.java
@@ -4,14 +4,11 @@ public class SerVivoComLevel extends SerVivo{
   public double level;
 
   public double obterExperienciaMaxima(){
-    
-    if (level == 100) return 0;
-    
-    switch(classeNumero){
-      case 1: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 267) / ((level + 4) * 6) ); break;
-      case 2: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 270) / ((level + 4) * 6.1) ); break;
-      case 3: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 270)/ ((level + 4) * 6.2) ); break;
-      default: return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 600)/ ((level + 4) * 9) ); break;
+
+    if (this.level >= 100){
+      return  999999999;
     }
+
+    return Math.round(Math.floor(Math.pow(level + 4, 4.017) + 267) / ((level + 4) * 6) );
   }
 }
